8049e84cd472407817774d623e25b96ea101d4ef
PivDen2000 <pivden2000@gmail.com>
2022-10-15 00:34:48 +0300

    Added stream-service.

---
+++ stream-service/js/index.js
@@ -0,0 +1,84 @@
+let Call = class {
+  constructor(outgoingNumber, incomingNumber, duration) {
+    this.outgoingNumber = outgoingNumber;
+    this.incomingNumber = incomingNumber;
+    this.duration = duration;
+  }
+};
+
+var calls = [
+  new Call('1', '1', '1s'),
+  new Call('1', '2', '1s'),
+  new Call('1', '3', '1s'),
+  new Call('1', '4', '1s'),
+  new Call('1', '5', '1s'),
+]
+
+var currentStream = -1
+var MAX_LIST_ITEMS = 200
+
+function onload () {
+  var streamSelect = document.getElementById('streamSelect')
+  for (var i = 0; i < streams.stream.length; i++) {
+    var opt = streams.stream[i].name
+    var li = document.createElement('li')
+    var lia = document.createElement('a')
+    lia.textContent = opt
+    lia.value = opt
+    lia.id = i
+    lia.setAttribute('class', 'dropdown-item')
+    lia.setAttribute('href', '#')
+    li.appendChild(lia)
+    streamSelect.appendChild(li)
+  }
+
+  $('.dropdown-menu li a').on('click', function () {
+    var id = $(this).attr('id')
+    onStreamSelect(id)
+  })
+}
+
+function onStreamSelect (selectedStream) {
+  if (selectedStream != currentStream) {
+    currentStream = selectedStream
+    document.getElementById('dropdownMenuLink').textContent =
+        streams.stream[currentStream].name
+    $('#description').html('' + streams.stream[currentStream].description)
+
+    if (currentStream == 0) {
+      onPause()
+      return
+    }
+
+    switch (streams.stream[currentStream].id) {
+      case 'callnetwork':
+        for (let i = 0; i < calls.length; i++) {
+          setTimeout(addFormattedMessageCallNetwork,1000*(i+1), calls[i]);
+        }
+        break
+    }
+  }
+}
+
+function addFormattedMessageCallNetwork(call) {
+  var li = document.createElement('li')
+  li.setAttribute('class', 'list-group-item')
+  var outputHtml = ''
+  outputHtml +=
+      "<b>Source: </b>Call Network <i class='fa-solid fa-phone fa-2xl' style='float:right'></i><br>"
+  outputHtml += '<b>Outgoing Number: </b>' + call.outgoingNumber + '<br>'
+  outputHtml += '<b>Incoming Number: </b>' + call.incomingNumber + '<br>'
+  outputHtml += '<b>Call duration: </b>' + call.duration + '<br>'
+  li.innerHTML = outputHtml
+
+  var list = document.getElementById('messageList')
+  list.insertBefore(li, list.firstChild)
+
+  capElements()
+}
+
+function capElements () {
+  var list = document.getElementById('messageList')
+  if (list.childElementCount > MAX_LIST_ITEMS)
+    list.removeChild(list.lastElementChild)
+}
